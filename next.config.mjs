a83837e92a9f5eaa5163a4062e50b3748292594a
Aldong23 <aldongmolano08@gmail.com>
2026-03-16 21:09:51 +0800

    updated the base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   reactCompiler: true,
-  output: "export",
-  basePath: "/pangasinan-heritage-portal",
-  assetPrefix: "/pangasinan-heritage-portal/"
 };
 
 export default nextConfig;
