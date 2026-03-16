7f771ed1e70eaefc63bf5b74b7c4b731cbca21ad
Aldong23 <aldongmolano08@gmail.com>
2026-03-16 21:20:31 +0800

    Updates

--- _next/static/UYfOtb73laSdqBCNfP10e/_buildManifest.js
+++ _next/static/UYfOtb73laSdqBCNfP10e/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/pangasinan-heritage-portal//_next/:path+",
-        "destination": "/pangasinan-heritage-portal/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
